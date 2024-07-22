5f79c8249eb55ef9586628c13891c338dbe47027
julianbolanoscode <julianbp97@hotmail.com>
2024-07-21 20:21:26 -0500

    Initial commit

---
+++ TC_02.03 Validar el inicio de sesión.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class TC0203ValidareliniciodesesinTest {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @Before
+  public void setUp() {
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void tC0203Validareliniciodesesin() {
+    driver.get("https://test-qa.inlaze.com/auth/sign-in");
+    driver.findElement(By.id("email")).sendKeys("pedropablo@email.com");
+    driver.findElement(By.cssSelector(".w-full > #password")).sendKeys("a");
+    driver.findElement(By.cssSelector(".btn:nth-child(3)")).click();
+  }
+}
---
+++ TC_02.06 Validar home page.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class TC0206ValidarhomepageTest {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @Before
+  public void setUp() {
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void tC0206Validarhomepage() {
+    driver.get("https://test-qa.inlaze.com/auth/sign-in");
+    driver.findElement(By.id("email")).sendKeys("pedropablo@email.com");
+    driver.findElement(By.cssSelector(".w-full > #password")).sendKeys("Mayo98+.");
+    driver.findElement(By.cssSelector(".btn:nth-child(3)")).click();
+    driver.findElement(By.cssSelector("img")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+}
